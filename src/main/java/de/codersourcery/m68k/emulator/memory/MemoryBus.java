ea5f4da99d4c67edeb4e9e37c7b71a80c0f1e4fe
Tobias Gierke <tobias.gierke@code-sourcery.de>
2018-09-01 14:01:52 +0200

    wip

--- src/main/java/de/codersourcery/m68k/emulator/memory/MemoryBus.java
+++ src/main/java/de/codersourcery/m68k/emulator/memory/MemoryBus.java
@@ -2,90 +2,81 @@ package de.codersourcery.m68k.emulator.memory;
 
 public class MemoryBus
 {
-    private final Memory memory;
+/*
 
-    private boolean isFree;
-
-    private int address;
-    private int value;
-
-    private IReader reader;
-    private IWriter writer;
-
-    public static final int EP_DENISE = 1<<0;
-    public static final int EP_PAULA = 1<<1;
-    public static final int EP_AGNUS = 1<<2;
-    public static final int EP_CPU = 1<<3;
-
-    public abstract class IReader
+                 BIT#  FUNCTION    DESCRIPTION
+                 ----  ---------   -----------------------------------
+                 15    SET/CLR     Set/clear control bit. Determines
+                                   if bits written with a 1 get set or
+                                   cleared.  Bits written with a zero
+                                   are unchanged.
+                 14    BBUSY       Blitter busy status bit (read only)
+                 13    BZERO       Blitter logic  zero status bit
+                                   (read only).
+                 12    X
+                 11    X
+                 10    BLTPRI      Blitter DMA priority
+                                   (over CPU micro) (also called
+                                   "blitter nasty") (disables /BLS
+                                   pin, preventing micro from
+                                   stealing any bus cycles while
+                                   blitter DMA is running).
+                 09    DMAEN       Enable all DMA below
+                 08    BPLEN       Bitplane DMA enable
+                 07    COPEN       Copper DMA enable
+                 06    BLTEN       Blitter DMA enable
+                 05    SPREN       Sprite DMA enable
+                 04    DSKEN       Disk DMA enable
+                 03    AUD3EN      Audio channel 3 DMA enable
+                 02    AUD2EN      Audio channel 2 DMA enable
+                 01    AUD1EN      Audio channel 1 DMA enable
+                 00    AUD0EN      Audio channel 0 DMA enable
+ */
+    public enum Owner
     {
-        public abstract void dataReady(int value);
+        BITPLANE_DMA,
+        COPPER_DMA,
+        BLITTER_DMA,
+        SPRITE_DMA,
+        DISK_DMA,
+        AUDIO0_DMA,
+        AUDIO1_DMA,
+        AUDIO2_DMA,
+        AUDIO3_DMA,
     }
 
-    public abstract class IWriter
-    {
-        public abstract void writeCompleted();
+    private int cyclesOccupied;
+    private Owner owner;
+
+    public Owner getOwner() {
+        return owner;
     }
 
-    private int delay;
-
-    public MemoryBus(Memory memory) {
-        this.memory = memory;
+    public boolean hasOwner(Owner o) {
+        return this.owner == o;
     }
 
     public boolean isFree() {
-        return isFree;
+        return owner == null;
     }
 
-    public void write(int address, int value,IWriter writer)
+    public boolean isOccupied()
     {
-        this.isFree = false;
-        this.address = address;
-        this.value = value;
-        this.delay = 2;
-        this.writer = writer;
+        return owner != null;
     }
 
-    public void read(int address, IReader reader)
+    public boolean occupy(Owner owner)
     {
-        this.isFree = false;
-        this.address = address;
-        this.delay = 2;
-        this.reader = reader;
+        this.owner = owner;
     }
 
     public void tick()
     {
-        if ( ! isFree )
+        if (cyclesOccupied > 0 )
         {
-            delay--;
-            if ( delay == 0 )
-            {
-                if ( reader != null )
-                {
-                    try
-                    {
-                        final int tmp = memory.readWord(address);
-                        reader.dataReady(tmp);
-                    }
-                    finally
-                    {
-                        reader = null;
-                    }
-                }
-                else
-                {
-                    try
-                    {
-                        memory.writeWord(address, value);
-                        writer.writeCompleted();
-                    }
-                    finally
-                    {
-                        writer = null;
-                    }
-                }
-                isFree = true;
+            cyclesOccupied--;
+            if ( cyclesOccupied == 0 ) {
+                owner = null;
             }
         }
     }
